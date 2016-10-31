c0b5ae534f036c6b463402df79e37e44e4860d2d
Dan Lindsay <dlindsay72@hotmail.com>
2016-10-31 15:15:17 -0400

    Can now edit alarms set and add or delete a picture to it

--- Alarmed/AlarmVC.swift
+++ Alarmed/AlarmVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AlarmVC: UITableViewController, UITextFieldDelegate {
+class AlarmVC: UITableViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var caption: UITextField!
@@ -21,7 +21,19 @@ class AlarmVC: UITableViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = alarm.name
+        name.text = alarm.name
+        caption.text = alarm.caption
+        datePicker.date = alarm.time
         
+        if alarm.image.characters.count > 0 {
+            //if we have an image, try to load it
+            let imageFileName = Helper.getDocumentsDirectory().appendingPathComponent(alarm.image)
+            
+            imageView.image = UIImage(contentsOfFile: imageFileName.path)
+            
+            tapToSelectImage.isHidden = true
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,15 +43,21 @@ class AlarmVC: UITableViewController, UITextFieldDelegate {
     
     @IBAction func datePickerChanged(_ sender: AnyObject) {
         
+        alarm.time = datePicker.date
     }
 
     @IBAction func imageViewTapped(_ sender: AnyObject) {
         
+        let vc = UIImagePickerController()
+        vc.modalPresentationStyle = .formSheet
+        vc.delegate = self
+        present(vc, animated: true)
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         
         alarm.name = name.text!
+        alarm.caption = caption.text!
         title = alarm.name
     }
     
@@ -49,5 +67,62 @@ class AlarmVC: UITableViewController, UITextFieldDelegate {
         return true
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        //dismiss the image picker
+        dismiss(animated: true)
+        
+        //fetch the image that was picked
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        
+        let fm = FileManager()
+        
+        if alarm.image.characters.count > 0 {
+            //the alarm already has an image, so delete it
+            do {
+                let currentImage = Helper.getDocumentsDirectory().appendingPathComponent(alarm.image)
+                
+                if fm.fileExists(atPath: currentImage.path) {
+                    try fm.removeItem(at: currentImage)
+                }
+            } catch {
+                print("Failed to remove current image")
+            }
+        }
+        do {
+           //generate a new filename for the image
+            alarm.image = "\(UUID().uuidString).jpg"
+            
+            //write a new image to the documents directory
+            let newPath = Helper.getDocumentsDirectory().appendingPathComponent(alarm.image)
+            
+            let jpeg = UIImageJPEGRepresentation(image, 80)
+            try jpeg?.write(to: newPath)
+        } catch {
+            print("Failed to save new image")
+        }
+        //update the user interface
+        imageView.image = image
+        tapToSelectImage.isHidden = true
+    }
 
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Alarmed/Helper.swift
@@ -0,0 +1,21 @@
+//
+//  Helper.swift
+//  Alarmed
+//
+//  Created by Dan Lindsay on 2016-10-31.
+//  Copyright © 2016 Dan Lindsay. All rights reserved.
+//
+
+import Foundation
+
+struct Helper {
+    
+    static func getDocumentsDirectory() -> URL {
+        
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        let documentsDirectory = paths[0]
+        
+        return documentsDirectory
+    }
+}
